9ecd7198270aabbd9e056f6311dfb744f84774ee
Moreldev237 <Moreldev237@gmail.com>
2026-03-12 10:39:04 +0100

    change default databse to POSTGRESQL

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("<h1>Hello, World! <span> i am NKONGA MOREL aspiring devops engineer </span></h1>" \
+    "this is my first project")
 
